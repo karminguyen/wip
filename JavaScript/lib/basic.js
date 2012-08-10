bcdd896fef6adf7880c527736532590b921dad3c
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2012-08-11 00:04:43 +0200

    Added a number of examples

--- JavaScript/lib/basic.js
+++ JavaScript/lib/basic.js
@@ -1,16 +1,46 @@
+//
 // Stolen, with pride, for misc sources. John Resig's Ninja book for instance.
+//
 
-// A simple logging statement that works in all browsers.
+(function(){
 
-function log() {
-  try {
-    console.log.apply( console, arguments );
-  } catch(e) {
-    try {
-      opera.postError.apply( opera, arguments );
-    } catch(e){
-      alert( Array.prototype.join.call( arguments, " " ) );
-    }
-  }
-}
+	//--------- Unit test functions ---------
+
+	var results;
+	
+	this.assert = function assert( value, desc ) {
+		var li = document.createElement("li");
+		li.className = value ? "pass" : "fail";
+		li.appendChild( document.createTextNode( desc ) );
+		results.appendChild( li );
+		if ( !value ) {
+			li.parentNode.parentNode.className = "fail";
+		}
+		return li;
+	};
+	
+	this.test = function test(name, fn) {
+		results = document.getElementById("results");
+		results = assert( true, name ).appendChild(
+			document.createElement("ul") );
+		fn();
+	};
+	
+
+	//--------- Logging ---------
+	
+	// A simple logging statement that works in all browsers.
+	this.log = function log() {
+	  try {
+	    console.log.apply( console, arguments );
+	  } catch(e) {
+	    try {
+	      opera.postError.apply( opera, arguments );
+	    } catch(e){
+	      alert( Array.prototype.join.call( arguments, " " ) );
+	    }
+	  }
+	}
+
+})();
 
