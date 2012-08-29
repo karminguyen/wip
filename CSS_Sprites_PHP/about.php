3e518b2776e790737bc9a9f9859e436606bb06ab
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2012-08-29 21:21:33 +0200

    Added CSS Sprites

---
+++ CSS_Sprites_PHP/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" href="css/default.css" />
+
+<title>Menu Test</title>
+
+</head>
+
+<body id="body_about">
+
+<div id="container">
+
+<ul id="nav">
+<li id="nav_home"><a href="index.php">Home</a></li>
+<li id="nav_about"><a href="about.php">About</a></li>
+<li id="nav_gallery"><a href="gallery.php">Gallery</a></li>
+<li id="nav_services"><a href="services.php">Services</a></li>
+<li id="nav_contact"><a href="contact.php">Contact</a></li>
+</ul>
+
+</div><!--end container-->
+
+</body>
+</html>
