5f846a521d6fc7c076089764487cd61852d98a42
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-01-12 21:27:23 +0100

    Fix

--- opencv/example1.cpp
+++ opencv/example1.cpp
@@ -1,8 +1,8 @@
-#include “highgui.h”
+#include "highgui.h"
 
 int main( int argc, char** argv ) {
-    IplImage* img = cvLoadImage( argv[1] ); cvNamedWindow( “Example1”, CV_WINDOW_AUTOSIZE ); cvShowImage( “Example1”, img );
+    IplImage* img = cvLoadImage( argv[1] ); cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE ); cvShowImage( "Example1", img );
     cvWaitKey(0);
     cvReleaseImage( &img );
-    cvDestroyWindow( “Example1” );
+    cvDestroyWindow( "Example1" );
 }
