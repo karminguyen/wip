166c2776c953124d12cc82bd6fbc3eeee0affbaa
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-02-27 20:57:09 +0100

    Added unit tests

---
+++ Python/first_env/fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import local
+
+#
+# This is a small fabfile developed for learning purposes
+#
+# Examples: 
+# >fab prepare_deploy
+# >fab hello:name=MYNAME
+#
+
+def hello(name="world"):
+    print("Hello %s!" % name)
+
+
+#
+# Run unit tests
+#
+# An alternative way to set PYTHONPATH is to edit bin/activate
+#
+
+def test():
+    local("PYTHONPATH=tests ./bin/python -m test1")
+
+
---
+++ Python/first_env/setup.py
@@ -0,0 +1,12 @@
+#!./bin/python
+
+from distutils.core import setup
+
+setup(name='MyApp',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Jonas Colmsjö',
+      author_email='jonas@gizur.com',
+      url='http://colmsjo.com',
+      packages=[],
+     )
